e4fd0033fdbe381498cce3c76b2ffa13c5a53592
echaabane <elias.chaabane@gmail.com>
2017-03-15 06:46:20 +0100

    Ajoutez des fichiers projet.

---
+++ Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherAssignment;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WeatherDataServiceFactory obj = WeatherDataServiceFactory.Instance;                     //get instance of weather data service factory
+
+            Location location = new Location();                                                     //create location object
+            location.city = "London";
+
+            var tmp = obj.GetWeatherDataService(location);
+            System.Console.WriteLine("Country: " + tmp.Sys.Country.ToString() + "\nCity: " + tmp.Name.ToString() + "\nTempreture: " + tmp.Main.Temp.ToString() + " (Kelvin)" + "\nWind Speed: " + tmp.Wind.Speed.ToString() + "\nPressure: " + tmp.Main.Pressure.ToString() + " (Kelvin)" + "\nHumidity: " + tmp.Main.Humidity.ToString());
+            System.Console.Read();
+        }
+    }
+}
---
+++ TDD/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WeatherAssignment;
+
+namespace TDD
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestGetWeatherData()
+        {
+            WeatherDataServiceFactory obj = WeatherDataServiceFactory.Instance;
+
+            Location location = new Location();
+            location.city = "Tel-Aviv";
+
+            CityWeather tmp = obj.GetWeatherDataService(location);                  //check if get worked
+            Assert.IsNotNull(tmp);
+        }
+
+        [TestMethod]
+        public void Test2GetWeatherData()
+        {
+            WeatherDataServiceFactory obj = WeatherDataServiceFactory.Instance;
+
+            Location location = new Location();
+            location.city = "Barcelona";
+
+            CityWeather tmp = obj.GetWeatherDataService(location);                  //check if get worked
+            Assert.IsNotNull(tmp);
+        }
+    }
+}
